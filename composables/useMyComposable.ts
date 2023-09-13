2dc9295ccbc298f2263b3f1f3d5ff9fb3d538a9b
dettes <stefan.dette@adesso.de>
2023-09-13 13:54:28 +0200

    first commit

---
+++ composables/useMyComposable.ts
@@ -0,0 +1,8 @@
+export const useMyComposable = () => {
+  // Because your composable is called in the right place in the lifecycle,
+  // useRuntimeConfig will also work
+  const config = useRuntimeConfig()
+  console.log('useMyComposable config', config)
+
+ return { config }
+}
